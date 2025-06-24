e4842646710fd3dd44698d051118ea1cd039a989
Rasmus Kemppainen <rasmus.kemppainen@student.lut.fi>
2025-06-24 13:46:59 +0300

    some new comments to wish

--- thirdProject/wish.c
+++ thirdProject/wish.c
@@ -5,6 +5,7 @@
 
 
 // Deepseek taught me to use global variable to allocate memory for the user input //
+// Global variable is easy to change afterwards, when the code expands //
 #define MAX 256
 
 // I learned here to list file directories //
@@ -17,15 +18,18 @@ int ls(char *path) {
 
     directory = opendir(dir_path);
 
+    // Check if the directory is null, then raise error message //
     if (directory == NULL) {
         printf("Error opening the directory.\n");
         return 0;
     }
-
+    // going through the entries and //
     while ((entry = readdir(directory)) != NULL) {
+        // checking if the entry is a file //
         if (entry->d_type == DT_REG) {
             printf("File: %s\n", entry->d_name);
         }
+        // or a directory //
         else if (entry->d_type == DT_DIR) {
             printf("dir: %s\n",  entry->d_name);
         }
@@ -43,6 +47,7 @@ int main(int argc, char *argv[]) {
     char input[MAX];
     /* Deepseek helped me to run while command checking number 1 and changing it to exit 0, instead of
        comparing the string with strcmp to "exit"  */
+    // printing the wish> to console interactively, until user types "exit" //
      if (argc < 2) {
         while (1) {
             printf("wish> ");
@@ -54,18 +59,21 @@ int main(int argc, char *argv[]) {
             if (strcmp(input, "exit") == 0) {
                 break;
             }
-            else if (strcmp(input, "ls")) {
+            /* Deepseek helped me fix a bug where ls didn't work, Deepseek suggested me to add
+             == 0 condition to the else if */
+            else if (strcmp(input, "ls") == 0) {
                 ls(NULL);
             }
         }
      }
      else {
+        // error handling when file doesn't exist //
         FILE *fp = fopen(argv[1], "r");
         if (fp == NULL) {
             perror("There was an error opening the file");
             return 0;
         }
-
+        // when user types exit, the system exits with 0 //
         while (fgets(input, sizeof(input), fp) != NULL) {
             input[strcspn(input, "\n")] = '\0';
             if (strcmp(input, "exit") == 0) {
